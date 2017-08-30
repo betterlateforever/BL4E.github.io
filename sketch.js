eb7e21c0ce60c5c3eb22127e121f37603b2518df
betterlateforever <31358525+betterlateforever@users.noreply.github.com>
2017-08-30 15:41:21 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,36 @@
 function setup() {
- createCanvas(300,300);
-background(155,155,155);
-
- stroke(200,200,200);
- fill(50,50,50);
- 
- stroke(0,0,0);
- fill(255,255,255,50);
- ellipse(150,150,100,100);
- ellipse(150,150,90,90);
- ellipse(150,150,80,90);
- ellipse(150,150,70,90);
- ellipse(150,150,60,90);
- ellipse(150,150,50,90);
- ellipse(150,150,40,90);
- ellipse(150,150,30,90);
- ellipse(150,150,20,90);
- ellipse(150,150,10,90);
- 
-  ellipse(150,150,90,10);
- ellipse(150,150,80,20);
- ellipse(150,150,70,30);
- ellipse(150,150,60,40);
- ellipse(150,150,50,50);
- ellipse(150,150,40,60);
- ellipse(150,150,30,70);
- ellipse(150,150,20,80);
- ellipse(150,150,10,90);
+ createCanvas(640,480);
 }
 
-function draw() {
+function draw(){
+background(155, 155, 155);
 
+  stroke(200, 200, 200);
+  fill(50, 50, 50);
+
+  stroke(0);
+  fill(50, 50, 255, 10);
+  ellipseMode(CENTER);
+
+  ellipse(mouseX,height/2,100,100);
+  ellipse(mouseX,height/2,90,90);
+  ellipse(mouseX,height/2,80,90);
+  ellipse(mouseX,height/2,70,90);
+  ellipse(mouseX,height/2,60,90);
+  ellipse(mouseX,height/2,50,90);
+  ellipse(mouseX,height/2,40,90);
+  ellipse(mouseX,height/2,30,90);
+  ellipse(mouseX,height/2,20,90);
+  ellipse(mouseX,height/2,10,90);
+
+fill(225, 50, 50, 10);
+  ellipse(width/2,mouseY,90,10);
+  ellipse(width/2,mouseY,80,20);
+  ellipse(width/2,mouseY,70,30);
+  ellipse(width/2,mouseY,60,40);
+  ellipse(width/2,mouseY,50,50);
+  ellipse(width/2,mouseY,40,60);
+  ellipse(width/2,mouseY,30,70);
+  ellipse(width/2,mouseY,20,80);
+  ellipse(width/2,mouseY,10,90);
 }
