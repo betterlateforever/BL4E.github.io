dbb6ebf9f9d132492b63231c2af9606125dbd88e
betterlateforever <betterlateforever@users.noreply.github.com>
2017-08-30 05:58:50 +0100

    Test Site Upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+ createCanvas(300,300);
+background(155,155,155);
+
+ stroke(200,200,200);
+ fill(50,50,50);
+ 
+ stroke(0,0,0);
+ fill(255,255,255,50);
+ ellipse(150,150,100,100);
+ ellipse(150,150,90,90);
+ ellipse(150,150,80,90);
+ ellipse(150,150,70,90);
+ ellipse(150,150,60,90);
+ ellipse(150,150,50,90);
+ ellipse(150,150,40,90);
+ ellipse(150,150,30,90);
+ ellipse(150,150,20,90);
+ ellipse(150,150,10,90);
+ 
+  ellipse(150,150,90,10);
+ ellipse(150,150,80,20);
+ ellipse(150,150,70,30);
+ ellipse(150,150,60,40);
+ ellipse(150,150,50,50);
+ ellipse(150,150,40,60);
+ ellipse(150,150,30,70);
+ ellipse(150,150,20,80);
+ ellipse(150,150,10,90);
+}
+
+function draw() {
+
+}
