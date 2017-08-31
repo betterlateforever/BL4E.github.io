de3dc744216691553cede58b0a963714a534565b
betterlateforever <31358525+betterlateforever@users.noreply.github.com>
2017-08-31 01:25:30 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,27 +9,27 @@ function setup() {
     fill(50, 50, 255, 25);
     ellipseMode(CENTER);
 
-    ellipse(mouseX, height / 2, 100, 100);
-    ellipse(mouseX, height / 2, 90, 90);
-    ellipse(mouseX, height / 2, 80, 90);
-    ellipse(mouseX, height / 2, 70, 90);
-    ellipse(mouseX, height / 2, 60, 90);
-    ellipse(mouseX, height / 2, 50, 90);
-    ellipse(mouseX, height / 2, 40, 90);
-    ellipse(mouseX, height / 2, 30, 90);
-    ellipse(mouseX, height / 2, 20, 90);
-    ellipse(mouseX, height / 2, 10, 90);
+    ellipse(mouseX, mouseY, 100, 100);
+    ellipse(mouseX, mouseY, 90, 90);
+    ellipse(mouseX, mouseY, 80, 90);
+    ellipse(mouseX, mouseY, 70, 90);
+    ellipse(mouseX, mouseY, 60, 90);
+    ellipse(mouseX, mouseY, 50, 90);
+    ellipse(mouseX, mouseY, 40, 90);
+    ellipse(mouseX, mouseY, 30, 90);
+    ellipse(mouseX, mouseY, 20, 90);
+    ellipse(mouseX, mouseY, 10, 90);
 
     fill(225, 50, 50, 25);
-    ellipse(width / 2, mouseY, 90, 10);
-    ellipse(width / 2, mouseY, 80, 20);
-    ellipse(width / 2, mouseY, 70, 30);
-    ellipse(width / 2, mouseY, 60, 40);
-    ellipse(width / 2, mouseY, 50, 50);
-    ellipse(width / 2, mouseY, 40, 60);
-    ellipse(width / 2, mouseY, 30, 70);
-    ellipse(width / 2, mouseY, 20, 80);
-    ellipse(width / 2, mouseY, 10, 90);
+    ellipse(width-mouseX, mouseY, 90, 10);
+    ellipse(width-mouseX, mouseY, 80, 20);
+    ellipse(width-mouseX, mouseY, 70, 30);
+    ellipse(width-mouseX, mouseY, 60, 40);
+    ellipse(width-mouseX, mouseY, 50, 50);
+    ellipse(width-mouseX, mouseY, 40, 60);
+    ellipse(width-mouseX, mouseY, 30, 70);
+    ellipse(width-mouseX, mouseY, 20, 80);
+    ellipse(width-mouseX, mouseY, 10, 90);
   }
   
   function mousePressed (){
